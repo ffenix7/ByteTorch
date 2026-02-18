a4ba3f4c78653220fffef26c2f42f6312a5cb7df
Filip Gębala <filip.gebala@interia.pl>
2026-02-18 12:16:56 +0100

    feat: added LayerNorm2D

--- src/nn/layernorm.py
+++ src/nn/layernorm.py
@@ -1,9 +1,9 @@
 import numpy as np
-from src.core.tensor import Tensor, Parameter
+from src.core.tensor import Parameter
 from src.nn.module import Module
 
 class LayerNorm1D(Module):
-    def __init__(self, size, eps = 1e-05):
+    def __init__(self, size, eps = 1e-08):
         super().__init__()
         self.size = size
         self.eps = eps
@@ -13,6 +13,26 @@ class LayerNorm1D(Module):
     def forward(self, input):
         mean = input.mean()
         std = input.std()
+        normalized = (input - mean) / (std + self.eps)
+        output = self.gamma * normalized + self.beta
+        return output
+
+class LayerNorm2D(Module):
+    """
+    Following PyTorch implementation, data is by default normalised along second axis.
+    Input size is supposed to be (B, C, L) or (B, C, W, H)
+    """
+    def __init__(self, size, eps = 1e-08, axis = 1):
+        super().__init__()
+        self.size = size
+        self.axis = axis
+        self.eps = eps
+        self.gamma = Parameter.ones(size, requires_grad=True)
+        self.beta = Parameter.zeros(size, requires_grad=True)
+
+    def forward(self, input):
+        mean = input.mean(axis = self.axis, keepdims = True)
+        std = input.std(axis= self.axis, keepdims = True)
         normalized = (input - mean) / (std + self.eps)
         output = self.gamma * normalized + self.beta
         return output
